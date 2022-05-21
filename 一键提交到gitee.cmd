e48f076fd70145780aea3f70b390a76bc47c7035
lxhcaicai <2778763221@qq.com>
2022-05-21 17:37:32 +0800

    Batch automatic push :2022/05/21,17:37:32

--- 一键提交到gitee.cmd
+++ 一键提交到gitee.cmd
@@ -4,17 +4,17 @@
 cd /d %~dp0
 :: 自动提交
 git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+ git commit -m "Batch automatic push :%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
-@echo 已经完成,
+@echo have completed,
 
 SET daoTime=60
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul
 cls
-echo 上传Git完成 ,倒计时退出: %daoTime%秒
+echo Git upload complete, countdown exit %daoTime%秒
 if %daoTime%==0 (exit) else (goto dao)
 
 
